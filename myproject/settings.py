670d734920ac1a10b84b38b64af1edeb150bbf1c
Maroof Gadiwale <gmaroof99@gmail.com>
2025-03-14 21:50:02 +0530

    requirement

--- myproject/settings.py
+++ myproject/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -121,7 +120,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
 STATICFILES_DIRS = (os.path.join(BASE_DIR,"static"),)
-django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
